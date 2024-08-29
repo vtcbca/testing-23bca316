2fa4a26b5af70546a01c829332ef5100463e6ce4
23bca316 <23bca316@vtcbcsr.edu.in>
2024-08-29 10:48:06 +0530

    Add files via upload

---
+++ Assignment/assignment1.py
@@ -0,0 +1,4 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def print_name(name):
+...     print(f"hello,{name}!")
